d772ed3c62a414fc496a5ce4c749ce8e2d4d9556
Tommy <sky.landtower@gmail.com>
2019-07-01 09:38:23 +0900

    up date

--- docs/js/lazy-form.js
+++ docs/js/lazy-form.js
@@ -1,6 +1,11 @@
   function _addListener(elm, type, func){ elm.addEventListener(type, func, false); }
   var e = function(p){return document.getElementById(p)};
-  e('s0').onfocus = function(){ var c = e('error').innerHTML; if(c) e('error').innerHTML = '';}
+  e('s0').onfocus = function(){if(e('error').innerHTML) e('error').innerHTML = '';}
+  e('s0').onblur = function(){
+    if( /@/i.test(this.value) && this.value.length > 8){
+        e('error').innerHTML = "よろしければ【配信登録】または【配信解除】をクリックしてください";
+    }
+  }
   e('s1').onclick = ck;
   e("s2").onclick = ck;
   function ck(){
@@ -10,7 +15,6 @@
       el.innerHTML = "メールアドレスは必須です";
       return false;
     }
-    //*** 適正なメールアドレスではない
     if(! /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(m)) {
       el.innerHTML = "正しいメールアドレスを入力してください";
       return false;
